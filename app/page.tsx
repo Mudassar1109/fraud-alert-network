0551ca89cc71ac6063d11b86db21cb84d836b79b
Mudassar1109 <elahimudassasr1@gmail.com>
2026-06-18 05:50:36 -0700

    First version

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Fraud Alert Network
+      </h1>
+
+      <p className="mt-4 text-lg">
+        Protecting people from online scams worldwide.
+      </p>
+    </main>
+  );
+}
