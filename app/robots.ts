01ccefc81b5bc0a82581aaca023bae7afae6b468
Mudassar1109 <elahimudassasr1@gmail.com>
2026-06-19 06:25:46 -0700

    Add robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://fraud-alert-network-1blz.vercel.app/sitemap.xml",
+  };
+}
